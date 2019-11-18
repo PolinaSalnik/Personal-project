cdc0b0cc86de11ade0dc18723ff7395e20959b5c
PolinaSalnik <polina.salnik.99@mail.ru>
2019-11-18 15:56:34 +0300

    add

--- src/js/base.js
+++ src/js/base.js
@@ -1,1 +1,15 @@
-console.log('Привет')
+  $(window).on('load', function () {
+    $preloader = $('.loaderArea'),
+      $loader = $preloader.find('.loader');
+    $loader.fadeOut();
+    $preloader.delay(850).fadeOut('slow');
+  });
+
+  
+  (function($){
+    $(function() {
+      $('.menu__icon').on('click', function() {
+        $(this).closest('.menu').toggleClass('menu_state_open');
+      });
+    });
+  })(jQuery);
