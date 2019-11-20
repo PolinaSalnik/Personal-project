e703b2fcd63cbac00bb7d63104f6319822e920ef
PolinaSalnik <polina.salnik.99@mail.ru>
2019-11-20 11:21:42 +0300

    add

--- src/js/base.js
+++ src/js/base.js
@@ -13,3 +13,27 @@
       });
     });
   })(jQuery);
+
+
+  function countup(className){
+		var countBlockTop = $("."+className).offset().top;
+		var windowHeight = window.innerHeight;
+		var show = true;
+					
+		$(window).scroll( function (){
+			if(show && (countBlockTop < $(window).scrollTop() + windowHeight)){ 
+				show = false;
+						
+				$('.'+className).spincrement({
+					from: 1,
+					duration: 4000,
+				});
+			}
+		})	
+	}
+
+	
+        $(function() {
+		countup("count", $(".count").text());
+		countup("count2", $(".count2").text());
+        });
