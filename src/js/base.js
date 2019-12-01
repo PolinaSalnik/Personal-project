e28d86a86c31f06a01cd0999707e1021942fea4e
PolinaSalnik <55506635+PolinaSalnik@users.noreply.github.com>
2019-12-01 11:38:31 +0300

    add

--- src/js/base.js
+++ src/js/base.js
@@ -148,3 +148,22 @@ form.reset();
     });
 });
 
+
+//BTN SCROLL TOP
+
+
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
+
